3226eb651735165609ac59805366521cb3bd4ca6
신동규 <donggyu9410@gmail.com>
2026-02-20 00:03:03 +0900

    feat

--- src/infrastructure/mongo_user_repository.py
+++ src/infrastructure/mongo_user_repository.py
@@ -1,6 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
+from src.domain.entities.user import User
+
 
 class MongoUserRepository:
     def __init__(self, db_client: AsyncIOMotorClient, db_name: str = "dailylog"):
-        self.collection: AsyncIOMotorCollection = db_client[db_name]["refresh_tokens"]
+        self.collection: AsyncIOMotorCollection = db_client[db_name]["users"]
+
+    async def create(self, user: User) -> User:
+        user_dict = user.model_dump(exclude={"id"})
+        result = await self.collection.insert_one(user_dict)
+        return User(**user_dict, id=str(result.inserted_id))
