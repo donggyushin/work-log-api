ffc2d39e69f7a05a6f5c78ba05871aa03fb143df
신동규 <donggyu9410@gmail.com>
2026-02-21 17:02:32 +0900

    feat

--- src/infrastructure/anthropic_ai_chat_bot.py
+++ src/infrastructure/anthropic_ai_chat_bot.py
@@ -2,6 +2,7 @@ from src.domain.entities.chat import ChatMessage, ChatSession
 from src.domain.interfaces.ai_chat_bot import AIChatBot
 
 
+# 구현 필요
 class AnthropicAIChatBot(AIChatBot):
     async def send(self, chat: ChatSession) -> ChatMessage:
         return await super().send(chat)
