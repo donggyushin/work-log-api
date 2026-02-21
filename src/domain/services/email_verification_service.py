85e1b44fc9431fb148abf27a87e8cabc66e6d885
신동규 <donggyu9410@gmail.com>
2026-02-21 14:34:31 +0900

    EmailVerificationService

--- src/domain/services/email_verification_service.py
+++ src/domain/services/email_verification_service.py
@@ -1,6 +1,22 @@
 from src.domain.entities.user import User
+from src.domain.interfaces.email_sender import EmailSender
+from src.domain.interfaces.verification_code_generator import VerificationCodeGenerator
 
 
 class EmailVerificationService:
+    def __init__(
+        self,
+        email_sender: EmailSender,
+        verification_code_generator: VerificationCodeGenerator,
+    ):
+        self.email_sender = email_sender
+        self.verification_code_generator = verification_code_generator
+
     async def send_verification_code(self, user: User):
-        pass
+        random_code = self.verification_code_generator.generate()
+        await self.email_sender.send_email(
+            sender="i need sender email",
+            to=user.email,
+            title="Verify Email",
+            contents=f"I need HTML code using {random_code}",
+        )
