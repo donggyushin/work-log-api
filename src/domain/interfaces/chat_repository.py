d23e5b0b62812257c384f39001c6053ee7c314c1
신동규 <donggyu9410@gmail.com>
2026-02-21 17:19:42 +0900

    add chat repository

---
+++ src/domain/interfaces/chat_repository.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from src.domain.entities.chat import ChatMessage, ChatSession
+
+
+class ChatRepository(ABC):
+    @abstractmethod
+    async def create_session(self, session: ChatSession) -> ChatSession:
+        pass
+
+    @abstractmethod
+    async def find_active_session(self) -> Optional[ChatSession]:
+        pass
+
+    @abstractmethod
+    async def add_message(self, session: ChatSession, message: ChatMessage):
+        pass
+
+    @abstractmethod
+    async def end_session(self, session: ChatSession):
+        pass
