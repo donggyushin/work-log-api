7843254af2af568fd04b4e818faa2e7d28a922f6
신동규 <donggyu9410@gmail.com>
2026-03-08 00:17:37 +0900

    감정 추가

--- src/domain/entities/diary.py
+++ src/domain/entities/diary.py
@@ -1,6 +1,14 @@
 from datetime import date, datetime
+from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, Field
+
+
+class Emotion(str, Enum):
+    GOOD = "good"
+    BAD = "bad"
+    GLOOMY = "gloomy"
+    NORMAL = "normal"
 
 
 class Diary(BaseModel):
@@ -14,3 +22,4 @@ class Diary(BaseModel):
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
     user_wrote_this_diary_directly: bool = Field(default=False)
+    emotion: Optional[Emotion] = Field()
